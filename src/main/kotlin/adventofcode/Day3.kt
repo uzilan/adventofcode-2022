e73a5b781d3696aedce508b7fd6b72d81b2d8288
Uzi Landsmann <ulandsmann@invidi.com>
2022-12-03 06:59:32 +0100

    added day 3

---
+++ src/main/kotlin/adventofcode/Day3.kt
@@ -0,0 +1,47 @@
+package adventofcode
+
+import kotlin.time.ExperimentalTime
+
+object Day3 {
+    fun part1(input: List<String>): Int {
+        return input
+            .map { it.chunked(it.length / 2) }
+            .map {
+                it.first().toSet()
+                    .intersect(it.last().toSet())
+                    .first()
+                    .code
+            }
+            .map { value(it) }
+            .sumOf { it }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.chunked(3)
+            .map {
+                it.first().toSet()
+                    .intersect(it[1].toSet())
+                    .intersect(it.last().toSet())
+                    .first()
+                    .code
+            }
+            .map { value(it) }
+            .sumOf { it }
+    }
+
+    private const val a = 'a'.code
+    private const val aa = 'A'.code
+
+    private fun value(it: Int): Int {
+        return if (it < a) it - aa + 27 else it - a + 1
+    }
+
+    @JvmStatic
+    @OptIn(ExperimentalTime::class)
+    fun main(args: Array<String>) {
+        val input = Utils.readLines("day3.txt")
+
+        Utils.printResult("part 1") { part1(input) }
+        Utils.printResult("part 2") { part2(input) }
+    }
+}
---
+++ src/test/kotlin/adventofcode/Day3Test.kt
@@ -0,0 +1,17 @@
+package adventofcode
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class Day3Test : StringSpec({
+    val input = Utils.readLines("day3test.txt")
+
+    "Day 3 part 1" {
+        Day3.part1(input) shouldBe 157
+    }
+
+    "Day 3 part 2" {
+        Day3.part2(input) shouldBe 70
+    }
+
+})
